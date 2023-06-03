59bd296a7e82d33729e4972c0cfc0d1284da7474
Kedar200 <Kedardeshmukh2003@gmail.com>
2023-06-03 17:18:21 +0530

    some changes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Api Connected");
+  res.send("Api Connected hi nihal");
 });
 
 app.listen(80, () => {
-    console.log("Server listening on port 80");
+  console.log("Server listening on port 80");
 });
