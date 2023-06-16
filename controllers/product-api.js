db725d920ef61f6899f410bd8ee6dccd20fbb97d
Kedar200 <Kedardeshmukh2003@gmail.com>
2023-06-16 23:47:10 +0530

    All Product related functions added

--- controllers/product-api.js
+++ controllers/product-api.js
@@ -1,46 +1,68 @@
-const Product = require("../models/products");
+const Product = require("../models/products")
 
 const add_product = async (req, res, next) => {
   try {
-    let product = Product(req.body);
-    await product.save();
+    let product = Product(req.body)
+    await product.save()
     res.send({
       message: "Product added!",
       data: product,
-    });
+    })
   } catch (error) {
-    next({ status: 400, message: error });
+    next({ status: 400, message: error })
   }
 };
 const update_product = async (req, res, next) => {
   try {
-    await Product.update(
-      { _id: req.body.id },
-      {
-        $set: {
-          name: req.body.name,
-        },
+    let response = await Product.updateOne({ _id: req.body._id }, {
+      $set: {
+        "name": req.body.name,
+        "brief": req.body.brief,
+        "category": "Electronic",
+        "desc": req.body.desc,
+        "options": [{
+          "resolution": req.body.options.resolution,
+          "color": req.body.options.color
+        }],
+        "brand": req.body.brand,
+        "image": req.body.image
       }
-    );
-    console.log(product);
+    })
     res.send({
       message: "Product updated!",
-      data: product,
-    });
+      response: response
+    })
   } catch (error) {
-    next({ status: 400, message: error });
+    next({ status: 400, message: error })
   }
 };
 const getproduct = async (req, res, next) => {
   try {
-    let product = await Product.findOne({ _id: req.body._id });
+    let product = await Product.findOne({ _id: req.body._id })
+    if (product === null) {
+      throw ("Not found")
+    }
     res.send({
       message: "Product!",
       data: product,
-    });
+    })
   } catch (error) {
-    next({ status: 400, message: error });
+    next({ status: 400, message: error })
   }
 };
 
-module.exports = { add_product, update_product, getproduct };
+const delete_product = async (req, res, next) => {
+  try {
+    let product = await Product.deleteOne({ _id: req.body._id })
+    if (product === null) {
+      throw ("Not found")
+    }
+    res.send({
+      message: "Product Deleted"
+    })
+  } catch (error) {
+    next({ status: 400, message: error })
+  }
+};
+
+module.exports = { add_product, update_product, getproduct, delete_product }
--- routes/products_api.js
+++ routes/products_api.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const express = require("express")
 const {
-  add_product,
-  update_product,
-  getproduct,
-} = require("../controllers/product-api");
-const route = express.Router();
+    add_product,
+    update_product,
+    getproduct,
+    delete_product
+} = require("../controllers/product-api")
+const route = express.Router()
 
-route.post("/add", add_product);
-route.put("/update", update_product);
-route.get("/myproduct", getproduct);
-// route.delete("/delete", delete_product);
+route.post("/add", add_product)
+route.put("/update", update_product)
+route.get("/myproduct", getproduct)
+route.delete("/delete", delete_product)
 
-module.exports = route;
+module.exports = route
